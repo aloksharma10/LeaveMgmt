f39142888627f9a6416a5f853596ca98f1e7acbb
aloksharma10 <aloksharma10969@gmail.com>
2023-09-21 09:04:21 +0530

    changing in middlware

--- middleware.js
+++ middleware.js
@@ -1,34 +1,37 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
-const isAdminRoute = (pathname) => pathname.startsWith("/admin");
+const isAdminRoute = (pathname) => pathname.includes("/admin");
 
 const checkAdminRole = (token) => token?.role === "admin";
 
-const middleware = (req) => {
-  const { token } = req.nextauth;
-  const { pathname } = req.nextUrl;
+export default withAuth(
+  function middleware() {
+    const { token } = req.nextauth;
+    const { pathname } = req.nextUrl;
 
-  if (isAdminRoute(pathname) && !checkAdminRole(token)) {
-    return NextResponse.rewrite(
-      new URL("/accessdenied?unauthorized=true", req.url)
-    );
+    if (isAdminRoute(pathname) && !checkAdminRole(token)) {
+      return NextResponse.rewrite(
+        new URL("/accessdenied?unauthorized=true", req.url)
+      );
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ req, token }) => {
+        if (!token) {
+          return false;
+        }
+        const { pathname } = req.nextUrl;
+        if (isAdminRoute(pathname) && !checkAdminRole(token)) {
+          return NextResponse.rewrite(
+            new URL("/accessdenied?unauthorized=true", req.url)
+          );
+        }
+        return Boolean(token);
+      },
+    },
   }
-};
-
-const authorized = ({ req, token }) => {
-  if (!token) {
-    return false;
-  }
-  const { pathname } = req.nextUrl;
-  if (isAdminRoute(pathname) && !checkAdminRole(token)) {
-    return NextResponse.rewrite(
-      new URL("/accessdenied?unauthorized=true", req.url)
-    );
-  }
-  return Boolean(token);
-};
-
-export default withAuth(middleware, { callbacks: { authorized } });
+);
 
 export const config = { matcher: ["/admin/:path*", "/user/:path*"] };
