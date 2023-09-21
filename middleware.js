ff7c186dbdbc2f66882b1edbb15d2e3cb528f1c1
aloksharma10 <aloksharma10969@gmail.com>
2023-09-21 09:19:43 +0530

    middlware update

--- middleware.js
+++ middleware.js
@@ -1,32 +1,30 @@
 import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-
+import { rewrite } from "@vercel/edge";
 const isAdminRoute = (pathname) => pathname.includes("/admin");
 
 const checkAdminRole = (token) => token?.role === "admin";
 
 export default withAuth(
-  function middleware() {
+  function middleware(req) {
     const { token } = req.nextauth;
     const { pathname } = req.nextUrl;
+    console.log("middleware", token, pathname);
 
     if (isAdminRoute(pathname) && !checkAdminRole(token)) {
-      return NextResponse.rewrite(
-        new URL("/accessdenied?unauthorized=true", req.url)
-      );
+      return rewrite(new URL("/accessdenied?unauthorized=true", req.url));
     }
   },
   {
     callbacks: {
       authorized: ({ req, token }) => {
+        console.log("token", token);
         if (!token) {
           return false;
         }
         const { pathname } = req.nextUrl;
+        console.log("pathname", pathname);
         if (isAdminRoute(pathname) && !checkAdminRole(token)) {
-          return NextResponse.rewrite(
-            new URL("/accessdenied?unauthorized=true", req.url)
-          );
+          return rewrite(new URL("/accessdenied?unauthorized=true", req.url));
         }
         return Boolean(token);
       },
