4e789ebd5bdbe7e21bbb16d13fbc58605aef68c0
aloksharma10 <aloksharma10969@gmail.com>
2023-10-08 09:30:37 +0530

    updating middleware

--- middleware.js
+++ middleware.js
@@ -50,4 +50,4 @@ export default withAuth(
   }
 );
 
-export const config = { matcher: ["/admin/:path*", "/user/:path*", "/api/:path*"] };
+export const config = { matcher: ["/admin/:path*", "/user/:path*"] };
